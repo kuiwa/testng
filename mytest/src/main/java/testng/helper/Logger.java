9e72171687ef42e1690f3fd7a911911a90b2a3be
ekuiwag <kui.k.wang@ericsson.com>
2016-08-12 11:14:37 +0800

    little modification

--- mytest/src/main/java/testng/helper/Logger.java
+++ mytest/src/main/java/testng/helper/Logger.java
@@ -1,13 +1,14 @@
 package testng.helper;
 
-import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.testng.annotations.Test;
 
 public class Logger {
 //
     final private static SimpleDateFormat date_format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss:SS");
-    
+
     @Test(groups = {"group2"})
     public static void print(String message) {
         Date date= new Date();
@@ -16,4 +17,3 @@ public class Logger {
         System.out.println(message);
     }
 }
-
